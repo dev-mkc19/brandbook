4372331057db6cabcb7717c17dbff4a6ecd8eea3
dev-mkc19 <mr.mkc19@hotmail.com>
2019-11-09 12:28:09 +0300

    Redefine dict: Result

--- uploader/logic.py
+++ uploader/logic.py
@@ -1,34 +1,38 @@
 from lxml import etree
 
-result = {}
 
-def add_entry(key, value=None):
-    if key in result: # если ключ существует
+
+def add_entry(dict, key, value = None):
+    if key in dict: # если ключ существует
         if value == None:
             pass
-            return 'ключ есть, новое значение None - пропускаем'
+            #'ключ есть, новое значение None - пропускаем'
+            return dict
         else:
-            if result.get(key) == None:
-                result.update({key: [value]})
-                return 'ключ есть, старое значение None - добавляем не None'
+            if dict.get(key) == None:
+                dict.update({key: [value]})
+                #'ключ есть, старое значение None - добавляем не None'
+                return dict
             else:
-                old_value = result.get(key)
+                old_value = dict.get(key)
                 old_value.append(value)
-                result.update({key: old_value})
-                return 'ключ есть, старое значение не None - добавляем в список'
-                print(result[key], result.get(key))
+                dict.update({key: old_value})
+                #'ключ есть, старое значение не None - добавляем в список'
+                return dict
     else:
         if value == None:
-            result.update({key: value})
-            return 'ключa нет - добавляем None'
+            dict.update({key: value})
+            #'ключa нет - добавляем None'
+            return dict
         else:
-            result.update({key: [value]})
-            return 'ключa нет - добавляем'
+            dict.update({key: [ value]})
+            #'ключa нет - добавляем'
+            return dict
 
 
 
 def HeaderCheck(filename):
-
+    result = {}
     tree = etree.parse(filename)
     root = tree.getroot()
     for dashboard in root.iter('dashboard'): #проходим по каждой ноде <dashboard>
@@ -49,62 +53,62 @@ def HeaderCheck(filename):
 
                         # проверка структуры заголовка
                         if header_check[0].get('param') == 'vert' and header_check[1].get('param') == 'horz':
-                            add_entry('Заголовок')
+                            result = add_entry(key = 'Заголовок', dict = result)
                         else:
-                            add_entry('Заголовок', ' в дашборде "{}" структура не соответствует'.format(dashboard.get('name')) )
+                            result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" структура не соответствует'.format(dashboard.get('name')) , dict = result)
 
                         # проверка высоты заголовка
                         if int(header_check[1].get('fixed-size')) == 44:
-                            add_entry('Заголовок')
+                            result = add_entry(key = 'Заголовок', dict = result)
                         else:
-                            add_entry('Заголовок', ' в дашборде "{}" высота не равна 44px'.format(dashboard.get('name')) )
+                            result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" высота не равна 44px'.format(dashboard.get('name')) , dict = result)
 
                         # проверка ширины логотипа
                         logo = sub_child.find('zone')
 
                         try:
                             if int(logo.get('fixed-size')) == 105:
-                                add_entry('Заголовок')
+                                result = add_entry(key = 'Заголовок', dict = result)
                             else:
-                                add_entry('Заголовок', ' в дашборде "{}" ширина логотипа не равна 105px'.format(dashboard.get('name')) )
+                                result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" ширина логотипа не равна 105px'.format(dashboard.get('name')), dict = result )
                         except:
-                            add_entry('Заголовок', ' в дашборде "{}" проверка ширины логотипа не удалась'.format(dashboard.get('name')) )
+                            result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" проверка ширины логотипа не удалась'.format(dashboard.get('name')), dict = result )
 
                         # проверка отступов логотипа
                         try:
                             if int(logo.find('..//zone-style/format[@attr="margin"]').get('value')) == 12:
-                                add_entry('Заголовок')
+                                result = add_entry(key = 'Заголовок')
                             else:
-                                add_entry('Заголовок', ' в дашборде "{}" внешние отступы логотипа не равны 12px'.format(dashboard.get('name')) )
+                                result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" внешние отступы логотипа не равны 12px'.format(dashboard.get('name')), dict = result )
                         except:
-                            add_entry('Заголовок', ' в дашборде "{}" проверка отступов логотипа не удалась'.format(dashboard.get('name')) )
+                            result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" проверка отступов логотипа не удалась'.format(dashboard.get('name')), dict = result )
 
                         # проверка центровки логотипа
                         try:
                             if int(logo.get('is-centered')) == 0:
-                                add_entry('Заголовок', ' в дашборде "{}" у логотипа не не стоит флаг "Center Image"'.format(dashboard.get('name')) )
+                                result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" у логотипа не не стоит флаг "Center Image"'.format(dashboard.get('name')), dict = result )
                             else:
                                 pass
                         except:
-                            add_entry('Заголовок')
+                            result = add_entry(key = 'Заголовок', dict = result)
 
                         # проверка Fit Image логотипа
                         try:
                             if int(logo.get('is-scaled')) == 1:
-                                add_entry('Заголовок')
+                                result = add_entry(key = 'Заголовок', dict = result)
                             else:
-                                add_entry('Заголовок', ' в дашборде "{}" у логотипа не не стоит флаг "Fit Image"'.format(dashboard.get('name')) )
+                                result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" у логотипа не не стоит флаг "Fit Image"'.format(dashboard.get('name')), dict = result )
                         except:
-                            add_entry('Заголовок', ' в дашборде "{}" проверка параметра "Fit Image" не удалась'.format(dashboard.get('name')) )
+                            result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" проверка параметра "Fit Image" не удалась'.format(dashboard.get('name')), dict = result )
 
                         # проверка фона #565C61
                         if sub_child.find('..//zone-style/format[@attr="background-color"]').get('value').upper() == '#565C61':
-                            add_entry('Заголовок')
+                            result = add_entry(key = 'Заголовок', dict = result)
                         else:
-                            add_entry('Заголовок', ' в дашборде "{}" у заголовка фон не "#565C61" '.format(dashboard.get('name')) )
+                            result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" у заголовка фон не "#565C61" '.format(dashboard.get('name')), dict = result )
 
                     else:
-                        add_entry('Заголовок', ' в дашборде "{}" структура не соответствует'.format(dashboard.get('name')) )
+                        result = add_entry(key = 'Заголовок', value = ' в дашборде "{}" структура не соответствует'.format(dashboard.get('name')) , dict = result)
 
 
                 else:
